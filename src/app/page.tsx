dba2cd3252b784650f2605236fac9edd77f9c104
harshbajani <harshbajani7@gmail.com>
2025-11-27 15:39:33 +0530

    feat: Initialize authentication system using better-auth with Drizzle ORM and add common UI components.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,66 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { authClient } from "@/lib/auth-client";
+import { toast } from "sonner";
 
 export default function Home() {
-  return <Button>hello world</Button>;
+  const { data: session } = authClient.useSession();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  const onSubmit = () => {
+    authClient.signUp.email(
+      {
+        email,
+        password,
+        name,
+      },
+      {
+        onError: () => {
+          toast.error("Failed to sign up");
+        },
+        onSuccess: () => {
+          toast.success("Signed up successfully");
+        },
+      }
+    );
+  };
+
+  if (session?.user) {
+    return (
+      <div className="flex flex-col p-4 gap-y-4">
+        <p>Logges in as {session.user.name}</p>
+        <Button onClick={() => authClient.signOut()}>Sign Out</Button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="p-4 flex flex-col gap-y-4">
+      <form onSubmit={onSubmit}>
+        <Input
+          placeholder="Name"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Input
+          placeholder="Email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button type="submit">Sign Up</Button>
+      </form>
+    </div>
+  );
 }
